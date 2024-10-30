8d12b1662f9a410a05a0243a523e278de9892192
Narayan Gawas <narayan.gawas@spit.ac.in>
2024-10-30 19:24:38 +0530

    fixed form size

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -38,16 +38,16 @@ const SignUpPage = () => {
     }
   };
   return (
-    <div className="flex h-full login-bg  items-center  justify-center md:justify-start px-0 lg:px-20 md:px-16">
+    <div className="flex items-center justify-center h-full px-0 login-bg md:justify-start lg:px-20 md:px-16">
       {/* <div className="mb-12 md:mb-0 md:w-6/12 md:p-5 lg:w-6/12">
         <img
           src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
           alt="Phone image"
         />
       </div> */}
-      <div className="md:w-8/12 lg:px-10 lg:w-5/12 md:mt-2 sm:w-8/12 glassmorphism ">
+      <div className="w-10/12 md:w-8/12 lg:px-10 lg:w-5/12 glassmorphism ">
         <form>
-          <div className="text-lg md:text-3xl font-bold mb-5">
+          <div className="mb-5 text-lg font-bold md:text-3xl">
             Sign Up with us
           </div>
           <div className="relative mb-6" data-te-input-wrapper-init>
@@ -93,7 +93,7 @@ const SignUpPage = () => {
             SIGN UP
           </button>
           {/* <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-            <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+            <p className="mx-4 mb-0 font-semibold text-center dark:text-neutral-200">
               OR
             </p>
           </div>
