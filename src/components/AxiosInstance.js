098996f20c66c01397632b9c5c9c8e24d2ffe20c
Narayan Gawas <narayan.gawas@spit.ac.in>
2024-11-02 09:40:32 +0530

    removed timeout

--- src/components/AxiosInstance.js
+++ src/components/AxiosInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const commonAxios = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
